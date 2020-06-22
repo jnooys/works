d684e34b6d35230ab62e6df9a304f54c001a4f55
seoyoon <seoyoon@seoyoon.local>
2020-06-23 00:46:52 +0900

    fixed untracked files

--- 2020/0617_카러플스타컵/event_200617/src/js/ui.mobile.js
+++
@@ -1,8 +0,0 @@
-import { initLink } from './ui.common';
-import ScrollActive from './lib/scroll-active';
-
-const navActive = new ScrollActive('.nav', {activePoint: {from: 50}, activeClass: 'show'});
-
-window.addEventListener('DOMContentLoaded', function() {
-	initLink(0, -50);
-})
